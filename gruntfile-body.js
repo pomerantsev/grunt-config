d9b98c6ed2a6d31c7bd2c8825bf57ce5c28807cf
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-04-03 16:45:58 +0400

    Initial commit

---
+++ gruntfile-body.js
@@ -0,0 +1,69 @@
+'use strict';
+
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    copy: {
+      js: {
+        expand: true,
+        cwd: 'app/scripts',
+        dest: '.tmp/scripts/',
+        src: '**/*.js'
+      }
+    },
+    traceur: {
+      options: {
+        experimental: true
+      },
+      files: {
+        expand: true,
+        cwd: 'app/scripts',
+        dest: '.tmp/scripts/',
+        src: '**/*.js'
+      }
+    },
+    clean: {
+      server: '.tmp'
+    },
+    connect: {
+      options: {
+        port: 9000,
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: [
+            '.tmp',
+            'app'
+          ]
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['app/scripts/**/*.js'],
+        tasks: ['traceur']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.livereload.options %>'
+        },
+        files: [
+          'app/**/*.html',
+          'app/styles/**/*.css',
+          '.tmp/scripts/**/*.js'
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask('serve', [
+    'clean:server',
+    'traceur',
+    'connect:livereload',
+    'watch'
+  ]);
+};
