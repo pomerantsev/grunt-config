9941a1acbdf0857e7a09fadbb5481eddf3b35a39
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-04-05 19:56:33 +0400

    Change connect address to allow access from other devices

--- gruntfile-body.js
+++ gruntfile-body.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        hostname: 'localhost',
+        hostname: '0.0.0.0',
         livereload: 35729
       },
       livereload: {
